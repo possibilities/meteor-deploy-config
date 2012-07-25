6a7afefdda014307adfd7e888d55df99fbe10068
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-25 02:34:47 -0400

    bump version

--- bin/mcfg.js
+++ bin/mcfg.js
@@ -5,7 +5,6 @@ var program = require('commander');
 var _ = require('underscore');
 
 program
-  .version('0.0.1')
   .option('-h, --host <host>', 'specify the server [yourapp.meteor.com]', 'localhost')
   .option('-p, --port <port>', 'specify the server [3333]', Number, 80)
   .parse(process.argv);
